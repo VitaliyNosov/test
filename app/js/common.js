38a2ab108262379f565058548e5c2cc483c1e008
VitaliyNosov <webdeveloperstudio2017@gmail.com>
2020-03-02 20:14:50 +0200

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,21 @@
+$(function() {
+
+	$("#tabs").tabs();
+
+
+
+
+	$('#li').hover(function () {
+		console.log('vfdvdvf');
+		$('#panel__menu').css("visibility : visible");
+	});
+
+
+
+	$(".popup").magnificPopup();
+
+	
+
+});
+
+
