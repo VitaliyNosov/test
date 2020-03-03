938f3f0c4eddc4fa0d087fbf3bf5620e31e3c615
VitaliyNosov <webdeveloperstudio2017@gmail.com>
2020-03-03 19:34:06 +0200

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,28 @@
 $(function() {
 
+	
+
+	// tabs
 	$("#tabs").tabs();
 
-
-
+	// popup
 	$(".popup").magnificPopup();
 
-	
+	// mobile menu
+
+	$('.menu__triger').click(function () {
+
+		$('#mobile__menu').slideToggle(500);
+	});
+	$(window).resize(function () {
+
+		if ($(window).width() > 500) {
+
+			$('#mobile__menu').removeAttr('style');
+		}
+	});
+
+
 
 });
 
